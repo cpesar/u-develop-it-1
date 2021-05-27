6d8f545531a433dad434004baff9cd237cb2b2fa
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-27 15:45:00 -0600

    CRUD

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Wolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Kathy', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Billy', 'Joel', 1),
+  ('Octavia', 'Dotel', 1),
+  ('Unica', 'Zurn', 1);
