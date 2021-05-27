623d80b7435a2e88bf7d0773cf74301ec936e42e
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-27 17:15:18 -0600

    added database calls to get, create, and delete candidates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-// Connect to database
+// CONNECT TO DATABASE
 const db = mysql.createConnection(
   {
     host: 'localhost',
@@ -26,19 +26,52 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
-  //returns all data in the candidates table
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+                  // RETURNS ALL CANDIDATES
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+                    // GET A SINGLE CANDIDATE
+                    //insert where id = __
+db.query(`SELECT * FROM candidates WHERE id = 4`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
 });
 
-// Default response for any other request (Not Found)
-    //HANDLES USER REQUESTS THAT AREN'T SUPPORTED BY THE APP
+
+                      // DELETE A CANDIDATE
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+
+                        // CREATE A CANDIDATE
+      // The PRIMARY KEY constraint protects the table from creating duplicate id's
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+
+          // Default response for any other request (Not Found)
+            //HANDLES USER REQUESTS THAT AREN'T SUPPORTED BY THE APP
 app.use((req, res) => {
   res.status(404).end();
 });
 
 
-  // function to test that the connection is working
+            // FUNCTION TO TEST THAT THE APP IS WORKING
 // app.get('/', (req, res) => {
 //   res.json({
 //     message: 'Hello World'
