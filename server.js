61fa72c82780e2d704943bb987debe4e3052c4bd
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-27 20:37:43 -0600

    added put route to update a candidate's party

--- server.js
+++ server.js
@@ -151,7 +151,36 @@ app.post('/api/candidate', ({ body }, res) => {
 });
 
 
+            //PUT REQUEST TO UPDATE A CANDIDATES PARTY
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
 
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
 
 
                   //GET ROUTE FOR ALL PARTIES
