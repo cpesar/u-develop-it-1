ec876d98edb1777cf16b2194288c53762dbe123d
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-27 19:19:20 -0600

    created a new parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,17 @@
+-- create a parties table
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  -- use text instead of VARCHAR because the description is not limited to a set number of characters
+  description TEXT
+);
+
+-- create a candidates table
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
+
+
