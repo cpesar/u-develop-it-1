26a00f1d238c67a3fcac38f7e7c48de7deb55cc3
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-27 20:17:42 -0600

    added party routes

--- server.js
+++ server.js
@@ -152,6 +152,66 @@ app.post('/api/candidate', ({ body }, res) => {
 
 
 
+
+
+                  //GET ROUTE FOR ALL PARTIES
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if(err){
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+
+
+                //GET ROUTE FOR A SINGLE PARTY
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if(err){
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+
+
+
+              //DELETE ROUTE
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if(err) {
+      res.status(400).json({ error: res.message });
+      //checks if anything was deleted
+    } else if (!result.affectedRows){
+      res.json ({
+        message: 'Party not found'
+      });
+    } else {
+      res.json ({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
  
       // The PRIMARY KEY constraint protects the table from creating duplicate id's
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
